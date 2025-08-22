b0c4a505dbc9beea02e3972778e39a1b488eea4a
ALEX BARANOSKI SPECK <alex.speck@alunos.sc.senac.br>
2025-08-21 21:33:19 -0300

    ultima alteração

---
+++ src/main/java/br/com/alex/dto/ValidateResponseDTO.java
@@ -0,0 +1,34 @@
+package br.com.alex.dto;
+
+import java.time.LocalDateTime;
+
+public class ValidateResponseDTO {
+
+    private boolean valid;
+    private String message;
+    private LocalDateTime validatedAt;
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getValidatedAt() {
+        return validatedAt;
+    }
+
+    public void setValidatedAt(LocalDateTime validatedAt) {
+        this.validatedAt = validatedAt;
+    }
+}
